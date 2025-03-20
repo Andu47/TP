51267bf55040e7f811b6b1a06ff1f04c19a99ea8
Andu47 <andugolovatai@gmail.com>
2025-03-20 15:57:20 +0200

    Gata tema3

--- Teme/TEMA3.c
+++ Teme/TEMA3.c
@@ -133,7 +133,7 @@ void pushModificare(char *newText, int row)
     }
 }
 
-modificare *popModificare() 
+modificare *popModificare()
 {
     if (top_modificare == -1)
     {
@@ -164,18 +164,88 @@ void displayModificare()
 void verificareFunctieUNDO()
 {
     //*Verificare pentru push pop display
-    char *p = malloc(10 * sizeof(char)); //trebuie verificat
+    char *p = malloc(10 * sizeof(char)); // trebuie verificat
     p = "andu";
-    pushModificare(p, 5); //primul el din stiva
+    pushModificare(p, 5); // primul el din stiva
     p = "CdaA";
-    pushModificare(p, 6); //al doilea el din stiva
-    displayModificare(); //afisam stiva (CdaA, andu)
-    modificare *aux; //verificam comanda pop
+    pushModificare(p, 6); // al doilea el din stiva
+    displayModificare();  // afisam stiva (CdaA, andu)
+    modificare *aux;      // verificam comanda pop
     if ((aux = popModificare()) != NULL)
     {
         printf("%s", aux->text);
     }
-    //!functia Undo este de fapt doar functia popModificare()
-    //Algoritmul poate fi implementat pentru un fisier text, comanda push fiind folosita (de ex.) la umplerea unui buffer de caractere 
+    //! functia Undo este de fapt doar functia popModificare()
+    // Algoritmul poate fi implementat pentru un fisier text, comanda push fiind folosita (de ex.) la umplerea unui buffer de caractere
 }
 
+/*Sa se implementeze cu ajutorul unei cozi un buffer care retine temporar informatiile
+primite de la diversi transmitatori (informatia este adaugata in coada) si le transmite mai apoi catre
+un receptor (informatia este eliminata din coada). Informatia primita in buffer are urmatoare
+structura: numar identificare transmitator (int), mesaj (sir de caractere de maxim 256 caractere).*/
+
+// La fel ca la problema anterioara, creem o structura specifica pentru problema
+
+typedef struct
+{
+    int id;
+    char text[256];
+} transmisie;
+
+transmisie bufT[MAX_LEN]; // bufferul care va memora transmisiile
+int rearT = -1, frontT = -1;
+
+// vom avea nevoie de o comanda de push pop si display
+
+void pushT(transmisie *newT)
+{ // spre deosebire de problema anterioara, am ales sa se dea push la o transmisie (nu la caracteristicile ei)
+    if (rearT == MAX_LEN - 1)
+    {
+        printf("Bufferul de transmisii este plin\n");
+        return;
+    }
+    else
+    {
+        bufT[++rearT] = *newT; // copiem newT in urmatoarea pozitie din buffer
+        if (frontT == -1)
+            frontT = 0;
+    }
+}
+
+transmisie *popT()
+{
+    if (frontT > rearT || frontT == -1)
+    {
+        printf("Bufferul de transmisii este gol\n");
+        return NULL;
+    }
+    else
+    {
+        return &bufT[frontT++];
+    }
+}
+
+void displayT()
+{
+    if (frontT > rearT || frontT == -1)
+    {
+        printf("Bufferul de transmisii este gol\n");
+    }
+    else
+    {
+        for (int i = frontT; i >= rearT; i++)
+        {
+            printf("ID %d: %s", bufT[i].id, bufT[i].text);
+        }
+    }
+}
+
+void verificareTransmitatori()
+{
+    // ar trebui sa mearga, fiind aceeasi structura ca inainte
+}
+int main()
+{
+    verificareFunctieUNDO();
+    verificareTransmitatori();
+}
